907662c12114675ed682351303b78363e8a8b2c6
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-09-06 13:40:44 -0300

    feat - add trailers component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,8 +1,11 @@
+import Trailers from '@components/trailers'
+
 const Sidebar = () => {
   return (
-    <div className="flex flex-row xl:flex-column w-1/4 h-full xl:border-r border-gray-800 m-0 p-0">
-      <div className="xl:border-b border-gray-800 w-full h-3/5"></div>
-      <div></div>
+    <div className="flex flex-row w-full xl:flex-column xl:w-1/4 h-full xl:border-r border-gray-800 m-0 p-0">
+      <div className="border-b xl:border-b border-gray-800 w-full h-[30rem] md:h-72 xl:h-[30rem] ">
+        <Trailers />
+      </div>
     </div>
   )
 }
