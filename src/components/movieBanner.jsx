ceda9de1a0f8fa49e2cc05b8829be2fd48fcb1f9
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-09-08 01:13:05 -0300

    styles - moviebanner

--- src/components/movieBanner.jsx
+++ src/components/movieBanner.jsx
@@ -6,6 +6,25 @@ const MovieBanner = ({ movie }) => {
   return (
     <div className="relative h-full w-full">
       <h2 className="text-white text-2xl font-bold px-2">{movie?.title}</h2>
+      <div className="flex flex-row items-center justify-between absolute z-10 rounded-md px-2 py-1  bg-transparent backdrop-blur-lg mt-4 ml-4 max-w-full  w-full md:w-full lg:w-[450px] xl:w-fit h-[35px]">
+        <div class="flex -space-x-2 p-2 overflow-hidden">
+          <Image
+            class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt="Personas mirando contenido"
+            width={32}
+            height={32}
+          />
+          <Image
+            class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt="Personas mirando contenido"
+            width={32}
+            height={32}
+          />
+        </div>
+        <span className="text-white">2 Amigos estan mirando</span>
+      </div>
       <Image
         src={movie?.backdrop_path}
         alt="Picture of the author"
