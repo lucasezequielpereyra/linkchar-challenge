f9b49130394f9bc75469155cbc786402107639fd
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-09-10 21:19:53 -0300

    fix - responsive trailers

--- src/components/trailers.jsx
+++ src/components/trailers.jsx
@@ -10,6 +10,13 @@ const Trailers = ({ random }) => {
   const dataMovies = useSelector(selectCurrentNewsMovies)
   const randomMovies = [dataMovies[n1], dataMovies[n2]]
 
+  const sliceTitle = title => {
+    if (title.length > 20) {
+      return title.slice(0, 20) + '...'
+    }
+    return title
+  }
+
   return (
     <div className="w-full h-full p-6 flex flex-col border-b border-gray-800 px-10 xl:px-4 2xl:px-10">
       <h3 className="text-white text-xl">Nuevos Trailers</h3>
@@ -24,19 +31,19 @@ const Trailers = ({ random }) => {
                 height={170}
                 className="rounded-xl -z-0 h-[170px] lg:w-[450px] xl:w-full 2xl:w-[380px] object-cover"
               />
-              <div className="flex flex-row items-center justify-between absolute z-10 rounded-xl  bg-videoBackground backdrop-blur-sm -mt-[70px] max-w-full  w-full md:w-full lg:w-[450px] xl:w-full h-[70px] bg-gray-200 ">
-                <div className="flex flex-row pl-6 gap-4">
-                  <div>
-                    <PlayCircleIcon className="h-12 w-12 text-white cursor-pointer" />
-                  </div>
+              <div className="flex flex-row items-center justify-around absolute z-10 rounded-xl  bg-videoBackground backdrop-blur-sm -mt-[70px] max-w-full  w-full md:w-full lg:w-[450px] xl:w-full h-[70px] bg-gray-200 ">
+                <div className="flex items-center ml-3">
+                  <PlayCircleIcon className="h-10 w-10  text-white cursor-pointer" />
+                </div>
+                <div className="flex flex-row pl-6 gap-4 w-full">
                   <div className="h-full w-full flex flex-col justify-center">
-                    <div className="text-xs text-white"> {movie?.title}</div>
+                    <div className="text-xs text-white"> {sliceTitle(movie?.title)}</div>
                     <div className="text-xs text-white">{movie?.release_date}</div>
                     <div className="text-xs text-white">{movie?.original_language}</div>
                   </div>
                 </div>
-                <div className="px-6 flex flex-row">
-                  <div className="w-0.5 mr-2 bg-gradient-to-t from-gray-500 via-gray-200 to-gray-500"></div>
+                <div className="pr-4 flex flex-row">
+                  <div className="w-0.5 mr-2 bg-gradient-to-t from-gray-500 20% via-gray-300 50% to-gray-500 80%"></div>
                   <div className="bg-white px-2 rounded-lg text-sm">01:29</div>
                 </div>
               </div>
