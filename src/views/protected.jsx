c3909530ad9705e1b5a58e3f3020438b3c9df3a7
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-09-11 00:59:59 -0300

    update - add try catch

--- src/views/protected.jsx
+++ src/views/protected.jsx
@@ -15,16 +15,20 @@ const Protected = ({ children }) => {
       const session = await supabase.auth.getSession()
       const userSession = session?.data?.session
       if (userSession) {
-        const { data: users } = await supabase.from('users').select('*')
-        const user = users?.find(user => user.id === userSession.user.id)
-        if (user) {
-          // save user data to redux
-          dispatch(getFavGenres(user.favGenres))
-          dispatch(getFavMovies(user.favMovies))
-        } else {
-          await supabase
-            .from('users')
-            .insert({ id: userSession.user.id, favGenres: [], favMovies: [] })
+        try {
+          const { data: users } = await supabase.from('users').select('*')
+          const user = users?.find(user => user.id === userSession.user.id)
+          if (user) {
+            // save user data to redux
+            dispatch(getFavGenres(user.favGenres))
+            dispatch(getFavMovies(user.favMovies))
+          } else {
+            await supabase
+              .from('users')
+              .insert({ id: userSession.user.id, favGenres: [], favMovies: [] })
+          }
+        } catch (error) {
+          console.log(error)
         }
       }
     }
